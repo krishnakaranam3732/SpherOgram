605c78bad511e3ce80be13bdccecc84f0e89095f
krishnakaranam <karanam.s@husky.neu.edu>
2017-05-16 23:05:49 -0400

    modified app.js

--- test/app.js
+++ test/app.js
@@ -9,7 +9,7 @@ module.exports = function(app)
         var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
         var password = process.env.MLAB_PASSWORD_WEBDEV;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds143241.mlab.com:43241/heroku_xptmg5hr'; // user yours
     }
 
     var mongoose = require("mongoose");
