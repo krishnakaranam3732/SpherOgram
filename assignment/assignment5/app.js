0a9ae210776ba7a935225650f8b691f4386227da
krishnakaranam <karanam.s@husky.neu.edu>
2017-07-21 14:39:08 -0400

    assignment5

--- assignment/assignment5/app.js
+++ assignment/assignment5/app.js
@@ -13,8 +13,15 @@ module.exports = function (app) {
 
     // Local connection String
     var connectionString = "mongodb://localhost/webdev_summer_2017";
+    if (process.env.MLAB_USERNAME && process.env.MLAB_PASSWORD) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
 
     mongoose.connect(connectionString, {
         useMongoClient: true
-    });;
+    });
 };
